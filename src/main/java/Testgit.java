65e28fc12fc2aa64aeb5c9285d284ba990c53e3e
fuhuajvzi <fuhuajvzi@136.cn>
2019-02-22 19:44:54 +0800

    把项目添加到git中

---
+++ src/main/java/Testgit.java
@@ -0,0 +1,13 @@
+public class Testgit {
+    public static void main(String[] args) {
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+    }
+}
